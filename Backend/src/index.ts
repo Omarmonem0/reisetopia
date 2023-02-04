5a2abddf46bf3e7a65c565d0f733af743e115036
Omarmonem0 <omar.abdelmeneamahmedabdelkhalek@agoda.com>
2023-02-04 20:37:21 +0700

    Adjust index file

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,19 +1,26 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { HotelsController } from './Controller/HotelsController';
+import { Database } from './Config/Database';
+import { LocaleMiddleware } from './Middlewares/LocaleMiddleware';
+import { RequestTypeMiddleware } from './Middlewares/RequestTypeMiddleware';
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+Database.loadData()
 const hotelsController = new HotelsController()
 
 app.get('/', (req: Request, res: Response) => {
   res.json("Healthy!")
 });
 
-app.get('/v1/recruiting/hotels', hotelsController.index.bind(hotelsController))
-app.get('/v1/recruiting/hotels/:hotelId', hotelsController.show.bind(hotelsController))
+app.get('/v1/recruiting/hotels', RequestTypeMiddleware.extractRequestType,
+  LocaleMiddleware.extractLocale,
+  hotelsController.index.bind(hotelsController))
+app.get('/v1/recruiting/hotels/:hotelId', LocaleMiddleware.extractLocale, hotelsController.show.bind(hotelsController))
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
