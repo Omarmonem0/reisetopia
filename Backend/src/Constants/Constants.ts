6c3bc2e374d73eedcd232acfc35c365b44f8d4e5
Omarmonem0 <omar.abdelmeneamahmedabdelkhalek@agoda.com>
2023-02-06 15:09:43 +0700

    Add status codes to constants file

--- Backend/src/Constants/Constants.ts
+++ Backend/src/Constants/Constants.ts
@@ -1,8 +1,11 @@
-import { type } from "os"
-
 export const requestType = {
     search: "search",
     index: "index"
 }
 
 export type language = "en-US" | "es-ES" | "de-DE" | "fr-FR"
+
+export const statusCodes = {
+    sucess: 200,
+    internalServerError: 500
+}
